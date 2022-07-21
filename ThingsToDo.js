37954d207b084b1da32f953f6635d9a3914b78ff
Datta0237 <dattapatil9764@gmail.com>
2022-07-21 21:48:28 +0530

    added day3 ass

---
+++ ThingsToDo.js
@@ -0,0 +1,37 @@
+
+
+let puneImage=["https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/59/ac/b6/lavasa-international.jpg?w=400&h=-1&s=1","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/92/46/b5/img-20151121-094118-largejpg.jpg?w=400&h=300&s=1","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/16/21/4f/photo4jpg.jpg?w=400&h=300&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/a6/8e/74/terrace.jpg?w=400&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/e9/69/c8/aga-khan-palace.jpg?w=400&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/02/15/5c/ba/bale-killa.jpg?w=400&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/a9/9f/6e/phoenix-market-city.jpg?w=400&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/0f/bc/51/mulshi.jpg?w=400&h=300&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/0f/bc/51/mulshi.jpg?w=400&h=300&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/41/06/33/pune.jpg?w=500&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/86/55/d5/mandir-view-from-south.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/02/2b/9e/4c/small-temple.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/21/5e/d8/torna-fort.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/21/5e/d8/torna-fort.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/54/ae/a2/water-pouring-down-the.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/54/ae/a2/water-pouring-down-the.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/a9/14/19/img-20190804-wa0021-largejpg.jpg?w=800&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/ef/e7/2f/malshej-ghats.jpg?w=1100&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/ef/e7/2f/malshej-ghats.jpg?w=1100&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/60/91/76/caption.jpg?w=800&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/7b/50/04/tamhini-ghat.jpg?w=600&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/97/d3/a8/img-20180701-141140-largejpg.jpg?w=700&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/6b/bc/7c/img-20190721-wa0014-largejpg.jpg?w=900&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ea/f2/ac/very-beautiful-waterfall.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/a2/20/65/tamhini-ghat.jpg?w=1200&h=-1&s=1"];
+
+
+document.querySelector("rightimg").addEventListener("click",imagechange);
+
+function imagechange(){
+    console.log("hiii")
+}
+
+
+    
+
+    
--- signin.js
+++ signin.js
@@ -1,19 +1,24 @@
-let signupData=JSON.parse(localStorage.getItem("userData"));
+let signupData=JSON.parse(localStorage.getItem("userData")) ||[];
 
 document.querySelector("form").addEventListener("submit",loginfunc);
 
 function loginfunc(){
     event.preventDefault();
-    console.log("done")
     let useremail=document.querySelector("#email").value;
     let userpass=document.querySelector("#pass").value;
+    let res=false;
     signupData.forEach(function(ele,index){
+        
         if(useremail===ele.emailID && userpass===ele.password){
-            alert("succesfully logged in");
-            window.location.href="file:///C:/Users/datta/OneDrive/Desktop/WEB%2020%20101/Project-Code/index.html";
-        }else{
-            alert("Please Enter Valid Credentials");
-            
+            res=true;
         }
     })
+    if(res){
+        alert("succesfully logged in");
+        window.location.href="file:///C:/Users/datta/OneDrive/Desktop/WEB%2020%20101/Project-Code/index.html";
+       
+    }else{
+        alert("Please Enter Valid Credentials");
+    }
+    
 }
